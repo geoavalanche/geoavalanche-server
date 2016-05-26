4fd6b121cb45211b77fd469d86bc835726b3fb92
StefanoMorroni <smorroni@hotmail.com>
2016-05-26 13:08:36 +0200

    fix

--- src/extension/ga-wps/wps-dangerindex/src/main/java/org/geoavalanche/wps/dangerindex/DangerIndex.java
+++ src/extension/ga-wps/wps-dangerindex/src/main/java/org/geoavalanche/wps/dangerindex/DangerIndex.java
@@ -10,9 +10,11 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.io.WKTReader;
+import java.util.HashMap;
 import org.geotools.data.collection.ListFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
@@ -30,18 +32,19 @@ import org.geotools.process.factory.DescribeResult;
 import org.geotools.process.factory.StaticMethodsProcessFactory;
 import org.geotools.text.Text;
 import org.geotools.util.KVP;
+import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.Name;
 
-
 public class DangerIndex extends StaticMethodsProcessFactory<DangerIndex> {
+
     private static final Logger LOG = Logger.getLogger(DangerIndex.class.getName());
 
     public DangerIndex() {
         super(Text.text("GeoAvalanche"), "geoavalanche", DangerIndex.class);
     }
-    
+
     @DescribeProcess(title = "DangerIndex", description = "DangerIndex")
     @DescribeResult(description = "FeatureCollection")
     public static SimpleFeatureCollection DangerIndex(
@@ -52,77 +55,105 @@ public class DangerIndex extends StaticMethodsProcessFactory<DangerIndex> {
             ) throws Exception {
         
         List<SimpleFeature> featuresList = new ArrayList<SimpleFeature>();
-                
-        SimpleFeatureTypeBuilder tb = new SimpleFeatureTypeBuilder();
-        tb.setName(featureCollection.getSchema().getName());
-        tb.setCRS(featureCollection.getSchema().getCoordinateReferenceSystem());
-        tb.add("THE_GEOM", Polygon.class);
-        tb.add("DANGERINDEX", String.class);
-        SimpleFeatureType targetSchema = tb.buildFeatureType();
-        SimpleFeatureBuilder fb = new SimpleFeatureBuilder(targetSchema);        
-        
-        LOG.info("input="+featureCollection);
+        SimpleFeatureBuilder fb = getSimpleFeatureBuilder(featureCollection);
+
         SimpleFeatureIterator itr = featureCollection.features();
         while (itr.hasNext()) {
             SimpleFeature feature = itr.next();
             if (feature.getDefaultGeometry() instanceof LineString) {
-                LineString theGeometry = (LineString)feature.getDefaultGeometry();
-                for (int x=1; x<theGeometry.getCoordinates().length; x++ ) {
-                    Coordinate[] points = new Coordinate[] { theGeometry.getCoordinates()[x-1], theGeometry.getCoordinates()[x] };
-                    LineString newLineString = new LineString(points, theGeometry.getPrecisionModel(), theGeometry.getSRID());
-                    Polygon newPolygon = (Polygon)geoBuffer(newLineString, distance, quadrantSegments, capStyle);
+                LineString theGeometry = (LineString) feature.getDefaultGeometry();
+                GeometryFactory theGeometryFactory = new GeometryFactory(theGeometry.getPrecisionModel(), theGeometry.getSRID());
+                for (int x = 1; x < theGeometry.getCoordinates().length; x++) {
+                    Coordinate[] points = new Coordinate[]{theGeometry.getCoordinates()[x - 1], theGeometry.getCoordinates()[x]};
+                    LineString newLineString = theGeometryFactory.createLineString(points);
+                    Polygon thePolygon = (Polygon) geoBuffer(newLineString, distance, quadrantSegments, capStyle);
+                    MultiPolygon theMultiPolygon = new MultiPolygon(new Polygon[] {thePolygon}, thePolygon.getFactory());
                     fb.reset();
-                    fb.set("THE_GEOM", newPolygon);
-                    fb.set("DANGERINDEX", fakeIndex());
-                    featuresList.add(fb.buildFeature(feature.getID()+"."+x));
-                }  
-            } else if (feature.getDefaultGeometry() instanceof Point){
-                Point theGeometry = (Point)feature.getDefaultGeometry();
-                Polygon newPolygon = (Polygon)geoBuffer(theGeometry, distance, quadrantSegments, capStyle);
+                    for (Property p : feature.getProperties()) {
+                        if (!p.getName().getLocalPart().equalsIgnoreCase("geometry")) {
+                            fb.set(p.getName().getLocalPart(), p.getValue());                            
+                        }
+                    }
+                    fb.set("the_geom", theMultiPolygon);
+                    fb.set("dangerindex", fakeIndex());
+                    featuresList.add(fb.buildFeature(feature.getID() + "." + x));
+                }
+            } else if (feature.getDefaultGeometry() instanceof Point) {
+                Point theGeometry = (Point) feature.getDefaultGeometry();
+                Polygon thePolygon = (Polygon) geoBuffer(theGeometry, distance, quadrantSegments, capStyle);
+                MultiPolygon theMultiPolygon = new MultiPolygon(new Polygon[] {thePolygon}, thePolygon.getFactory());
                 fb.reset();
-                fb.set("THE_GEOM", newPolygon);
-                fb.set("DANGERINDEX", fakeIndex());
+                for (Property p : feature.getProperties()) {
+                    if (!p.getName().getLocalPart().equalsIgnoreCase("geometry")) {
+                        fb.set(p.getName().getLocalPart(), p.getValue());                            
+                    }
+                }
+                fb.set("the_geom", theMultiPolygon);
+                fb.set("dangerindex", fakeIndex());
                 featuresList.add(fb.buildFeature(feature.getID()));
             } else {
                 fb.reset();
-                fb.set("THE_GEOM", feature.getDefaultGeometry());
-                fb.set("DANGERINDEX", fakeIndex());
-                SimpleFeature newfeature = null;
-                newfeature = fb.buildFeature(feature.getID());
-                featuresList.add(newfeature);
+                for (Property p : feature.getProperties()) {
+                    fb.set(p.getName().getLocalPart(), p.getValue());
+                }
+                fb.set("dangerindex", fakeIndex());
+                featuresList.add(fb.buildFeature(feature.getID()));
             }
         }
 
-        SimpleFeatureCollection ret = new ListFeatureCollection(targetSchema, featuresList);
-        LOG.info("return=" +ret);        
+        SimpleFeatureCollection ret = new ListFeatureCollection(fb.getFeatureType(), featuresList);
+        LOG.info("return=" + ret);
         return ret;
-    }  
-          
-    private static Geometry geoBuffer(Geometry theGeometry,String distance,String quadrantSegments,String capStyle) throws Exception {
-        Name name = new NameImpl("geo","buffer");
-        Process process = Processors.createProcess( name );
+    }
+
+    static SimpleFeatureBuilder getSimpleFeatureBuilder(SimpleFeatureCollection featureCollection) {
+        Map<String, Class> theProperties = new HashMap();
+        SimpleFeatureIterator itr = featureCollection.features();
+        while (itr.hasNext()) {
+            SimpleFeature feature = itr.next();
+            for (Property p : feature.getProperties()) {
+                theProperties.put(p.getName().getLocalPart(), p.getType().getBinding());
+            }
+        }
+        theProperties.put("the_geom", MultiPolygon.class);
+        theProperties.put("dangerindex", String.class);
+
+        SimpleFeatureTypeBuilder tb = new SimpleFeatureTypeBuilder();
+        tb.setName(featureCollection.getSchema().getName());
+        tb.setCRS(featureCollection.getSchema().getCoordinateReferenceSystem());
+        for (Map.Entry<String, Class> entry : theProperties.entrySet()) {
+            tb.add(entry.getKey(), entry.getValue());
+        }
+        SimpleFeatureType schema = tb.buildFeatureType();
+        SimpleFeatureBuilder fb = new SimpleFeatureBuilder(schema);
+        return fb;
+    }
+
+    private static Geometry geoBuffer(Geometry theGeometry, String distance, String quadrantSegments, String capStyle) throws Exception {
+        Name name = new NameImpl("geo", "buffer");
+        Process process = Processors.createProcess(name);
         if (process == null) {
             throw new Exception("process is null");
         }
         ProcessExecutor engine = Processors.newProcessExecutor(2);
 
         // quick map of inputs
-        Map<String,Object> input = new KVP("geom", theGeometry, "distance", distance, "quadrantSegments", quadrantSegments, "capStyle", capStyle);
-        Progress working = engine.submit(process, input );
+        Map<String, Object> input = new KVP("geom", theGeometry, "distance", distance, "quadrantSegments", quadrantSegments, "capStyle", capStyle);
+        Progress working = engine.submit(process, input);
 
         // you could do other stuff whle working is doing its thing
-        if( working.isCancelled() ){
+        if (working.isCancelled()) {
             return null;
         }
 
-        Map<String,Object> result = working.get(); // get is BLOCKING
+        Map<String, Object> result = working.get(); // get is BLOCKING
         Geometry ret = (Geometry) result.get("result");
-        LOG.info("geo:buffer=" +ret);
-        return ret;       
+        LOG.info("geo:buffer=" + ret);
+        return ret;
     }
 
     private static String fakeIndex() {
-        String ret = ""+(System.currentTimeMillis()%3);
+        String ret = "" + (System.currentTimeMillis() % 3);
         return ret;
     }
 }
