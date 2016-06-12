a419beb95f734622d240112fba4d5a31e657b28d
StefanoMorroni <smorroni@hotmail.com>
2016-06-12 20:43:41 +0200

    mongodb (nosql database) integration

--- src/extension/ga-wps/wps-crowd/src/main/java/org/geoavalanche/wps/crowd/Crowd.java
+++ src/extension/ga-wps/wps-crowd/src/main/java/org/geoavalanche/wps/crowd/Crowd.java
@@ -1,14 +1,21 @@
 package org.geoavalanche.wps.crowd;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.geoWithin;
 import com.ushahidi.java.sdk.UshahidiApi;
-import com.ushahidi.java.sdk.api.Incident;
 import com.ushahidi.java.sdk.api.Incidents;
 import com.ushahidi.java.sdk.api.tasks.IncidentsTask;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -20,7 +27,7 @@ import org.geotools.data.simple.SimpleFeatureIterator;
 
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
-import org.geotools.geometry.DirectPosition2D;
+import org.geotools.geojson.geom.GeometryJSON;
 import org.geotools.geometry.jts.JTS;
 import org.geotools.process.factory.DescribeParameter;
 import org.geotools.process.factory.DescribeProcess;
@@ -31,7 +38,6 @@ import org.geotools.text.Text;
 import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.operation.MathTransform;
 
 public class Crowd extends StaticMethodsProcessFactory<Crowd> {
@@ -65,7 +71,11 @@ public class Crowd extends StaticMethodsProcessFactory<Crowd> {
                 theGeometry = (Geometry)feature.getAttribute("geometry");
             }
             if (theGeometry != null) {
-                fb.set("incidents", getNumIncidentsOfGeometry(incidents, theGeometry));
+                try {
+                    fb.set("incidents", getNumIncidentsOfGeometry(theGeometry));
+                } catch (Exception e) {
+                    fb.set("incidents", getNumIncidentsOfGeometry(incidents, theGeometry));
+                }
             } 
             featuresList.add(fb.buildFeature(feature.getID()));
         }
@@ -75,13 +85,13 @@ public class Crowd extends StaticMethodsProcessFactory<Crowd> {
         return ret;
     }
 
-    static int getNumIncidentsOfGeometry(List<Incidents> incidents, Geometry theGeometry) throws Exception {
+    static long getNumIncidentsOfGeometry(List<Incidents> incidents, Geometry theGeometry3857) throws Exception {
         boolean lenient = true;
         MathTransform transform = CRS.findMathTransform(CRS.decode("EPSG:3857"), CRS.decode("EPSG:4326"), lenient);
-        Geometry theGeometryWGS84 = JTS.transform(theGeometry, transform);
+        Geometry theGeometryWGS84 = JTS.transform(theGeometry3857, transform);
         
         GeometryFactory theGeometryFactory = new GeometryFactory(theGeometryWGS84.getPrecisionModel(), theGeometryWGS84.getSRID());            
-        int _incidents = 0;
+        long _incidents = 0;
         for (Incidents entry: incidents) {
             Coordinate theCoordinate = new Coordinate(entry.incident.getLongitude(), entry.incident.getLatitude());
             Point thePoint = theGeometryFactory.createPoint(theCoordinate);
@@ -89,6 +99,34 @@ public class Crowd extends StaticMethodsProcessFactory<Crowd> {
                 _incidents++;
             }
         }        
+        return _incidents;
+    }
+
+    static long getNumIncidentsOfGeometry(Geometry theGeometry3857) throws Exception {
+        long _incidents = 0;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        
+        boolean lenient = true;
+        MathTransform transform = CRS.findMathTransform(CRS.decode("EPSG:3857"), CRS.decode("EPSG:4326"), lenient);
+        Geometry theGeometryWGS84 = JTS.transform(theGeometry3857, transform);
+
+        GeometryJSON gjson = new GeometryJSON();
+        StringWriter writer = new StringWriter();
+        gjson.write(theGeometryWGS84, writer);
+        String json = writer.toString();
+        
+        String mongourl = "mongodb://localhost:27017";
+        MongoClient mongoClient = new MongoClient(new MongoClientURI(mongourl));
+        MongoDatabase db = mongoClient.getDatabase("test");
+        org.bson.Document theDocument = org.bson.Document.parse(json);        
+        MongoCursor<org.bson.Document> cur = db.getCollection("incidents").find(geoWithin("loc", theDocument)).iterator();
+        while(cur.hasNext()) {
+            org.bson.Document doc = cur.next();
+            org.bson.Document incident = (org.bson.Document)doc.get("incident");
+            Date incidentdate = sdf.parse(incident.getString("incidentdate"));
+            _incidents++;
+        }
+        mongoClient.close(); 
         return _incidents;
     }
     
@@ -101,7 +139,7 @@ public class Crowd extends StaticMethodsProcessFactory<Crowd> {
                theProperties.put(p.getName().getLocalPart(), p.getType().getBinding());
             }
         }
-        theProperties.put("incidents", Integer.class);
+        theProperties.put("incidents", Long.class);
 
         SimpleFeatureTypeBuilder tb = new SimpleFeatureTypeBuilder();
         tb.setName(featureCollection.getSchema().getName());
