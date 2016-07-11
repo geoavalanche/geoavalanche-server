a1b765114ee0684e8abcd08194946e1ae0cd3069
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2016-07-11 16:37:44 +0200

    Add logs when catching an exception

--- src/extension/ga-wps/wps-slope/src/main/java/org/geoavalanche/wps/slope/Slope.java
+++ src/extension/ga-wps/wps-slope/src/main/java/org/geoavalanche/wps/slope/Slope.java
@@ -7,11 +7,13 @@ import es.unex.sextante.core.OutputObjectsSet;
 import es.unex.sextante.core.ParametersSet;
 import es.unex.sextante.core.Sextante;
 import es.unex.sextante.dataObjects.IRasterLayer;
+import es.unex.sextante.exceptions.GeoAlgorithmExecutionException;
 import org.geoserver.wps.sextante.GTOutputFactory;
 import org.geoserver.wps.sextante.GTRasterLayer;
 import es.unex.sextante.morphometry.slope.SlopeAlgorithm;
 import es.unex.sextante.outputs.Output;
 import es.unex.sextante.outputs.OutputRasterLayer;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.processing.CoverageProcessor;
@@ -94,66 +96,78 @@ public class Slope extends StaticMethodsProcessFactory<Slope> {
         //Resulting values in radians
         int unit = SlopeAlgorithm.UNITS_RADIANS;
         
-        /*
-         * To use this data we need to wrap it with an object
-         * that implements the IRasterLayer, so SEXTANTE algorithms
-         * can access it.
-         * Since it is a Geotools object, we will use the Geotools
-         * wrapper class GTRasterLayer
-         */
-        GTRasterLayer raster = new GTRasterLayer();
-        raster.create(cropCov);
+        GridCoverage2D ret = null;
         
-        /*
-         * Instantiate the SlopeAlgorithm class
-         */
-        SlopeAlgorithm alg = new SlopeAlgorithm();
-        
-        /*
-         * The first thing we have to do is to set up the input parameters
-         */
-        ParametersSet params = alg.getParameters();
-        params.getParameter(SlopeAlgorithm.DEM).setParameterValue(raster);
-        params.getParameter(SlopeAlgorithm.METHOD).setParameterValue(method);
-        params.getParameter(SlopeAlgorithm.UNITS).setParameterValue(unit);
-        
-        /*
-         *  This algorithm will generate a new raster layer.
-         * We can select "where" to put the result. To do this, we
-         * retrieve the output container and set the output channel,
-         * which contains information about the destiny of the resulting
-         * data. The most common way of using this is setting
-         * a FileOutputChannel, which contains the information needed to
-         * put the output to a file (basically a filename).
-         * If we omit this, a FileOutputChannel will be used,
-         * using a temporary filename.
-         */
-        OutputObjectsSet outputs = alg.getOutputObjects();
-	Output out = outputs.getOutput(SlopeAlgorithm.SLOPE);
-        
-        OutputRasterLayer resSlope = (OutputRasterLayer) out.getOutputObject();
-        
-        /*
-         * Execute the algorithm. We use no task monitor,
-         * so we will not be able to monitor the progress
-         * of the execution. SEXTANTE also provides a DefaultTaskMonitor,
-         * which shows a simple progress bar, or you could make your
-         * own one, implementing the ITaskMonitor interface
-         *
-         * The execute method returns true if everything went OK, or false if it
-         * was canceled. Since we are not giving the user the chance to
-         * cancel it (there is no task monitor), we do not care about the
-         * return value.
-         *
-         * If something goes wrong, it will throw an exception.
-         */
-        alg.execute(null, outputFactory);
-        
-        IRasterLayer slope = (IRasterLayer) resSlope;
-    
-        GridCoverage2D ret = new GridCoverage2D("slopes", (GridCoverage2D) slope);
+        try {
+            /*
+             * To use this data we need to wrap it with an object
+             * that implements the IRasterLayer, so SEXTANTE algorithms
+             * can access it.
+             * Since it is a Geotools object, we will use the Geotools
+             * wrapper class GTRasterLayer
+             */
+            GTRasterLayer raster = new GTRasterLayer();
+            raster.create(cropCov);
+
+            /*
+             * Instantiate the SlopeAlgorithm class
+             */
+            SlopeAlgorithm alg = new SlopeAlgorithm();
+
+            /*
+             * The first thing we have to do is to set up the input parameters
+             */
+            ParametersSet params = alg.getParameters();
+            params.getParameter(SlopeAlgorithm.DEM).setParameterValue(raster);
+            params.getParameter(SlopeAlgorithm.METHOD).setParameterValue(method);
+            params.getParameter(SlopeAlgorithm.UNITS).setParameterValue(unit);
+
+            /*
+             *  This algorithm will generate a new raster layer.
+             * We can select "where" to put the result. To do this, we
+             * retrieve the output container and set the output channel,
+             * which contains information about the destiny of the resulting
+             * data. The most common way of using this is setting
+             * a FileOutputChannel, which contains the information needed to
+             * put the output to a file (basically a filename).
+             * If we omit this, a FileOutputChannel will be used,
+             * using a temporary filename.
+             */
+            OutputObjectsSet outputs = alg.getOutputObjects();
+            Output out = outputs.getOutput(SlopeAlgorithm.SLOPE);
+
+            OutputRasterLayer resSlope = (OutputRasterLayer) out.getOutputObject();
+
+            /*
+             * Execute the algorithm. We use no task monitor,
+             * so we will not be able to monitor the progress
+             * of the execution. SEXTANTE also provides a DefaultTaskMonitor,
+             * which shows a simple progress bar, or you could make your
+             * own one, implementing the ITaskMonitor interface
+             *
+             * The execute method returns true if everything went OK, or false if it
+             * was canceled. Since we are not giving the user the chance to
+             * cancel it (there is no task monitor), we do not care about the
+             * return value.
+             *
+             * If something goes wrong, it will throw an exception.
+             */
+            alg.execute(null, outputFactory);
+
+            IRasterLayer slope = (IRasterLayer) resSlope;
+
+            ret = new GridCoverage2D("slopes", (GridCoverage2D) slope);
+            
+        } catch (GeoAlgorithmExecutionException ex) {
+            
+            LOG.log(Level.SEVERE, "An exception in the GeoAlgorithm has been thrown {0}", ex.toString());
+            
+        } catch (Exception e) {
+            
+            LOG.log(Level.SEVERE, "A generic exception has been thrown {0}", e.toString());
+            
+        }
         return ret;
-        
     }
     
 }
